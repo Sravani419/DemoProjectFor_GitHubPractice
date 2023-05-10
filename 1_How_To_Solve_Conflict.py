9bcae1371bb4c12ca2c06739e02a20abbe66d668
Sravani419 <krishnamraju105@gmail.com>
2023-05-10 10:09:22 +0530

    first commit

---
+++ 1_How_To_Solve_Conflict.py
@@ -0,0 +1,30 @@
+
+
+Valaxy Technologies - youtube -> git tutorial - teaching is good
+
+https://www.youtube.com/watch?v=nDRWhKc5Yd4
+
+
+##what is conflict?
+Conflicts occur when two or more people change the same file(s) at the same time.
+The version control system does not allow people to overwrite each other's changes blindly, but highlights conflicts so that they can be resolved.
+
+##or
+
+A conflict arises when two separate branches have made edits to the same line in a file,
+or when a file has been deleted in one branch but edited in the other. Conflicts will most likely happen when working in a team environment.
+
+
+##or
+
+Often, merge conflicts happen when people make different changes to the same line of the same file,
+or when one person edits a file and another person deletes the same file. You must resolve all merge conflicts before you can merge a pull request on GitHub
+
+
+
+# How to solve conflict
+##Youtube
+https://www.youtube.com/watch?v=JtIX3HJKwfo
+
+#what is a branch?
+A branch is a set of code changes with the unique name 
